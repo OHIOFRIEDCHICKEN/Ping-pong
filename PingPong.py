13529adfd98d10d93c642101d50723e229d126d6
OHIOFRIEDCHICKEN <william.pham.0501@gmail.com>
2023-02-18 12:59:48 +0700

    Update Ping pong game

--- PingPong.py
+++ PingPong.py
@@ -15,16 +15,16 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update(self):
         keys_pressd = key.get_pressed()
-        if keys_pressed[K_UP] and self.rect.y > self.speed:
+        if keys_pressd[K_UP] and self.rect.y > self.speed:
             self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y:
+        if keys_pressd[K_DOWN] and self.rect.y:
             self.rect.y += self.speed
 
     def update2(self):
         keys_pressd = key.get_pressed()
-        if keys_pressed[K_w] and self.rect.y > self.speed:
+        if keys_pressd[K_w] and self.rect.y > self.speed:
             self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y:
+        if keys_pressd[K_s] and self.rect.y:
             self.rect.y += self.speed
 
 
@@ -32,12 +32,10 @@ font.init()
 font2 = font.SysFont("Calibri", 30)
 window = display.set_mode((700, 500))
 display.set_caption("Ping Pong")
-background = transform.scale(image.load("galaxy.jpg"), (700, 500))
-
-
-
+background = transform.scale(image.load("Ping-pong/pingpong.jpg"), (700, 500))
+line1 = Player('Ping-pong/line1.jpg', 30, 200, 50, 150, 4)
+line2 = Player('Ping-pong/line2.jpg', 630, 200, 50, 150, 4)
 #parameters of the image sprite
-
 
 #game loop
 run = True
@@ -45,14 +43,14 @@ finish = False
 clock = time.Clock()
 FPS = 80
 
-
-
-
 while run:
-    
+
     if finish != True:
         window.blit(background,(0, 0))
+        line1.update2()
 
+    line1.reset()
+    line2.reset()
 
     display.update ()
     clock.tick(FPS)
